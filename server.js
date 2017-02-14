358681a30ca0097ad86652f4bd35dca11123851b
Aundrey Drummond <aundreydrummond@aundreys-mbp.attlocal.net>
2017-02-14 00:39:35 -0500

    Prep Repo for Heroku

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 //Create our App
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use((req, res, next)=>{
+  if(req.headers['x-forwarded-proto'] === 'http'){
+    next();
+  }else{
+    res.redirect(`http://${req.hostname}${req.url}`);
+  }
+});
 
 app.use(express.static('public'));
-app.listen(8080, function(){
-  console.log('Server Up');
+
+app.listen(PORT, function(){
+  console.log(`Server Up on ${PORT}`);
 });
